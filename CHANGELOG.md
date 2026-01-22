99eb4301c7c39db2976c450d46478a984825385f
Almos Rajnai <almos.rajnai@appmindful.com>
2026-01-22 20:20:16 +1300

    New macro for converting word sized decimal to BCD number.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
   new macros for dumping KickAssembler lists as bytes or words to the code
 - `kickass/list.asm`, `Kickass_List_DumpWordsLowHighToCode` - new macro for dumping KickAssembler word list as low and
   high bytes to the code.
+- `std_lib/binary.asm`, `StdLib_Binary_WordToPackedBCD` - new macro for converting word-sized integer to packed BCD.
 
 ### Fixed
 
--- includes/std_lib/binary.asm
+++ includes/std_lib/binary.asm
@@ -32,3 +32,43 @@
     adc  #$80
     rol
 }
+
+/**
+ * Convert a 16 bit integer into 3 byte long packed dest number.
+ *
+ * Created by Andrew Jacobs
+ * Source: https://codebase64.net/doku.php?id=base:more_hexadecimal_to_decimal_conversion
+ *
+ * Changes:
+ *   A and X registers, D flag (cleared)
+ *
+ * @param src source integer address (2 bytes, little endian).
+ * @param dest destination packed dest number address (3 bytes).
+ **/
+.macro StdLib_Binary_WordToPackedBCD(src, dest) {
+        sed		        //Switch to decimal mode
+        lda #$00        //Ensure the result is clear
+		sta dest+0
+		sta dest+1
+		sta dest+2
+
+        ldx #16         //Number of source bits (16)
+
+!convert:
+        asl src+0	    //Shift out one bit
+		rol src+1
+		lda dest+0	    //And add into result
+		adc dest+0
+		sta dest+0
+		lda dest+1	    //Propagating any carry
+		adc dest+1
+		sta dest+1
+		lda dest+2	    //...thru whole result
+		adc dest+2
+		sta dest+2
+
+        dex             //And repeat for next bit
+        bne !convert-
+
+        cld             //Back to binary
+}
